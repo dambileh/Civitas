e117ad4b7798c975e5557f122b2fe6a2181d53cc
hadiosbourne <hadi.shayesteh@a24group.com>
2017-09-20 18:31:20 +0200

    Removed empty line
    Removed empty line

--- server/civitas/index.js
+++ server/civitas/index.js
@@ -61,7 +61,6 @@ swaggerTools.initializeMiddleware(swaggerDoc, function callback(middleware) {
   subscriptionManager.initialize();
 
   // Start the server
-
   if(process.argv.indexOf("PORT") != -1){ //does PORT exist?
     serverPort = process.argv[process.argv.indexOf("PORT") + 1];
   }
