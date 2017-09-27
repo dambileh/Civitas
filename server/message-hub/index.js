b380fca6cc2da53174a9ca5b15a1407b0af63d1f
hadiosbourne <hsh_85@yahoo.com>
2017-09-27 21:58:04 +0200

    added the code to setup socket io
    added the code to setup socket io

---
+++ server/message-hub/index.js
@@ -0,0 +1,74 @@
+'use strict';
+var express = require('express');
+var path = require('path');
+var app = express();
+var http = require('http');
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
+var fs = require('fs');
+var serverPort = 4030;
+
+var config = require('config');
+var errorHandler = require('../libs/error/ErrorHandler');
+var subscriptionManager = require('./managers/SubscriptionManager');
+var processHelper = require('../libs/ProcessHelper');
+let socketManager = require('./managers/SocketManager');
+
+// swaggerRouter configuration
+var options = {
+    swaggerUi: '/swagger.json',
+    controllers: './controllers',
+    useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+/**
+ * Initialize the Swagger middleware.
+ *
+ * @param {object} swaggerDoc - The swagger document object
+ * @param {function} callback - The callback
+ *
+ * @author Kevin Feng <kevin.feng@gmail.com>
+ * @since  10 Sept 2017
+ */
+swaggerTools.initializeMiddleware(swaggerDoc, function callback(middleware) {
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
+
+    // Validate Swagger requests
+    app.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
+
+    // Serve the Swagger documents and Swagger UI
+    if (process.env.NODE_ENV !== 'ci') {
+        app.use(middleware.swaggerUi());
+    }
+
+    app.use(errorHandler.onError);
+
+    subscriptionManager.initialize();
+
+    // Start the server
+    if (process.argv[2]) {
+        serverPort = process.argv[2];
+    }
+
+    app.set('views', path.join(__dirname, 'views'));
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    let server = http.createServer(app).listen(serverPort, function () {
+        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        if (process.env.NODE_ENV !== 'ci') {
+            console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+        }
+    });
+    processHelper.handleProcessExit();
+
+    socketManager.setSocket(server);
+});
+
---
+++ server/message-hub/services/MessageService.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var resourceNotFoundError = require('../../libs/error/ResourceNotFoundError');
+var appUtil = require('../../libs/AppUtil');
+var logging = require('../utilities/Logging');
+var config = require('config');
+var _ = require('lodash');
+var internalEventEmitter = require('../../libs/InternalEventEmitter');
+var messageHubChannels = require('../../PubSubChannels').MessageHub;
+var errors = require('../../ErrorCodes');
+var constants = require('../../Constants');
+let socketManager = require('../managers/SocketManager');
+/**
+ * The User Service module
+ */
+module.exports = {
+  /**
+   * Updates a user
+   *
+   * @param {object} request - The request arguments passed in from the controller
+   */
+  sendMessage: async function sendMessage(request) {
+    socketManager.getSocket().emit("my-channel", request);
+
+    return internalEventEmitter.emit(
+      messageHubChannels.Internal.SendMessageCompletedEvent,
+      {
+        statusCode: 200,
+        body: {
+          status: request.message
+        }
+      }
+    );
+  }
+};
